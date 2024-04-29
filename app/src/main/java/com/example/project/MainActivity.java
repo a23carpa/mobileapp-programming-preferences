9cac7084d51bb648c18845998c180b971957f23b
Carl <a23carpa@student.his.se>
2024-04-29 13:14:37 +0200

    Creating button that starts the new activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.project;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences myPreferenceRef;
+    SharedPreferences.Editor myPreferenceEditor;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myPreferenceRef = getSharedPreferences("pref", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        button = findViewById(R.id.firstButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }

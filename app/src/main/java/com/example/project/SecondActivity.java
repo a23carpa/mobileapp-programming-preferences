57031145b39b20a0d5ca234c80264ea8d3145746
Carl <a23carpa@student.his.se>
2024-04-29 13:15:31 +0200

    Button that when pressed saves data inside the Edittext to a SharedOReference

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,17 +1,40 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+    SharedPreferences myPreferenceRef;
+    SharedPreferences.Editor myPreferenceEditor;
+
+    EditText edit;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_second);
+
+        button = findViewById(R.id.SaveButton);
+        edit = findViewById(R.id.editInput);
+
+        myPreferenceRef = getSharedPreferences("pref", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myPreferenceEditor.putString("MyAppPreferenceString", edit.getText().toString());
+                myPreferenceEditor.apply();
+
+            }
+        });
 
     }
 }

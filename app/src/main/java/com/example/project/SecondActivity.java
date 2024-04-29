5b0ef331bc9929e8ac3df0daada7cddf3bace72b
Carl <a23carpa@student.his.se>
2024-04-29 12:21:33 +0200

    Adding second activity

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_second);
+
+    }
+}
